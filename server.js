cff6698a0cc782f67c83b15a0866a7d3504d2f07
johndwatt <jdw23@protonmail.com>
2022-02-10 15:30:46 -0800

    adds logger middleware

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 // Initialize
 const express = require("express");
 const cors = require("cors");
+const env = require('dotenv');
 
 // Config
+env.config();
 const app = express();
 const PORT = 4000;
 
 // Middleware
 app.use(express.json());
 app.use(cors());
+app.use(require("./utils/logger"));
 
 // API routes
 
